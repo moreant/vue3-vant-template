d348839f6f473e89bafd2677021dec91116a449f
moreant <moreant@foxmail.com>
2021-10-02 18:02:35 +0800

    init

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { Button } from 'vant'
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+
+  const toHome = () => {
+    router.push({ path: '/' })
+  }
+</script>
+
+<template>
+  <h1>About</h1>
+  <Button plain type="primary" @click="toHome">Back Home</Button>
+</template>
+
+<style></style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vitePluginImp from 'vite-plugin-imp'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'vant',
+          style(name) {
+            if (/CompWithoutStyleFile/i.test(name)) {
+              // This will not import any style file
+              return false
+            }
+            return `vant/es/${name}/style/index.js`
+          }
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '/src')
+    }
+  }
+})
